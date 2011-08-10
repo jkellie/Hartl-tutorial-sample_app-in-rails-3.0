ab8dccb46950cedaeb18d939f16950eb76091556
Jason Kellie <jkellie@me.com>
2011-08-09 20:35:55 -0400

    Working user signin

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@
 #  created_at         :datetime
 #  updated_at         :datetime
 #  encrypted_password :string(255)
-#
+#  salt               :string(255)
 
 class User < ActiveRecord::Base
   attr_accessor   :password
@@ -35,10 +35,17 @@ class User < ActiveRecord::Base
     encrypted_password == encrypt(submitted_password)
   end
   
-  def User.authenticate(email, submitted_password)
-    user = find_by_email(email)
-    return nil  if user.nil?
-    return user if user.has_password?(submitted_password)
+  class << self
+    def User.authenticate(email, submitted_password)
+      user = find_by_email(email)
+      return nil  if user.nil?
+      return user if user.has_password?(submitted_password)
+    end
+    
+    def authenticate_with_salt(id, cookie_salt)
+      user = find_by_id(id)
+      (user && user.salt = cookie_salt) ? user : nil
+    end
   end
       
       
